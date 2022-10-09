ab007959046a1ed3eb70ecbf124e5cbdb9d5d19b
amc86 <amcuadrado1986@hotmail.com>
2022-10-09 22:12:21 +0200

    commit final

--- Pokedex/pokedex.js
+++ Pokedex/pokedex.js
@@ -5,11 +5,11 @@ async function pokemon() {
   const resPokemon = await res.json ();
   allPokemon.push(resPokemon);
 }} 
-const mapPokemon = (pokem) =>{
+  const mapPokemon = (pokem) =>{
     const mappedPokemons = pokem.map((poke) => ({
       id: poke.id,
       name: poke.name,
-      type: poke.types.map(type => type.type.name),
+      types: poke.types.map(type => type.type.name),
       height: poke.weight,
       weight: poke.weight,
       img: poke.sprites.other.home.front_default,
@@ -18,23 +18,56 @@ const mapPokemon = (pokem) =>{
   }
   const imprimirPokemons = (pokem) => {
     const ol$$ = document.querySelector("ol");
+    ol$$.className = "ole";
     const div$$ = document.createElement('div');
+    div$$.className = "target";
+    const divFondPoke$$ = document.createElement("div");
+    divFondPoke$$.className = "fond-Poke";
+    const divfondo$$ = document.createElement("div");
+    divfondo$$.className = "ima-fondo";
     const img$$ = document.createElement('img');
     const h1$$ = document.createElement('h1');
     const h2$$ = document.createElement('h2');
+    const divname$$ = document.createElement('div');
+    divname$$.className = "fondodatos"
     img$$.setAttribute("src", pokem.sprites.other.home.front_default); 
+    img$$.className = "ima";
     h1$$.textContent = pokem.name;
+    h1$$.className = "Depixel";
     h2$$.textContent = "nº" + pokem.id;
+    h2$$.className = "number";
+    const pokeimg$$ = document.createElement('img');
+    pokeimg$$.className = "symbol";
     ol$$.appendChild(div$$);
-    div$$.appendChild(img$$);
+    div$$.appendChild(divFondPoke$$)
+    divFondPoke$$.appendChild(divfondo$$);
+    divfondo$$.appendChild(img$$);
     div$$.appendChild(h1$$);
     div$$.appendChild(h2$$);
+    div$$.appendChild(divname$$);
+    divname$$.appendChild(h1$$);
+    divname$$.appendChild(h2$$);
+    divname$$.appendChild(pokeimg$$);
     document.body.appendChild(ol$$)
   }
-  const searchPoke = (nombre,mapPokemon) => {
-    const filterPoke = mapPokemon.filter( (pokemon) => pokemon.name.toLowerCase().includes(nombre.toLowerCase()));//, (pokemon) => pokemon.type.toLowerCase().includes(type.toLowerCase()));
-    console.log(filterPoke);
-    imprimirPokemons(filterPoke);
+  const searchPoke = (filter,mapPokemon) => { 
+    const ol$$ = document.querySelector("ol");
+    ol$$.innerHTML = "";
+    const filterPoke = mapPokemon.filter( /**(pokemon) => pokemon.name.toLowerCase().includes(filter.toLowerCase()),*/ (pokemon) => {
+    if (pokemon.name.toLowerCase().includes(filter.toLowerCase())) {
+        return true
+    }
+      for (let i = 0; i < pokemon.types.length; i++) {
+        let type = pokemon.types[i].type;
+        if (type["name"].toLowerCase() == filter.toLowerCase()) {
+          return true;
+        }
+      }
+      return false;
+  });
+    for (const pokemon of filterPoke) {
+      imprimirPokemons(pokemon); 
+    }
 }
   const setListener = (mappedPokemons) => {
     let btn$$ = document.querySelector("button");
@@ -44,9 +77,9 @@ const mapPokemon = (pokem) =>{
   async function init () {
     const imprimirPokemon = await pokemon();
     const mappedPokemons = mapPokemon(allPokemon);
-    console.log(mappedPokemons);
     for (const pokemon of allPokemon) {
-    imprimirPokemons(pokemon);
-  }}
-setListener(allPokemon);
+      imprimirPokemons(pokemon);
+    }
+   }
+  setListener(allPokemon);
   init();
